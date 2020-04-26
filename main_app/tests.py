44fd256cb0ee80fe8258c7aa7d91f04ed8d6f5b7
Hadiuzzaman <hadi.mmpi@gmail.com>
2020-04-27 02:08:41 +0600

    initialized our project

---
+++ main_app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+
+# Create your tests here.
+class MainAppTest(SimpleTestCase):
+    def test_home_view_exist(self):
+        response=self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+    def test_aboute_view_exist(self):
+        response=self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
+    
+  
